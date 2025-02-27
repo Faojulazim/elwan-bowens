2e5b9ea42003bbab78da7b364793c06f2dd42bbf
Faojul <faojulazim13@gmail.com>
2025-02-27 20:24:30 +0600

    first commit

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ const left = document.querySelector("#left");
 const interval = document.querySelector("#interval");
 const tabs = document.querySelectorAll("[data-tabs]");
 const services = document.querySelectorAll("[data-services]");
+const buildingForm = document.getElementById("buildingForm");
+const upgradesForm = document.getElementById("upgradesForm");
+const repairsForm = document.getElementById("repairsForm");
+
+//Hamburger Toggle Section
+
 hamburger.addEventListener("click", (e) => {
   hamburger.classList.add("hidden");
   closeIcon.classList.remove("opacity-0");
   closeIcon.classList.remove("rotate-[90deg]");
   left.classList.remove("translate-x-[-150%]");
 });
+
+//Close Icon Toggle Section
+
 closeIcon.addEventListener("click", (e) => {
   hamburger.classList.remove("hidden");
   closeIcon.classList.add("opacity-0");
   closeIcon.classList.add("rotate-[90deg]");
   left.classList.add("translate-x-[-150%]");
 });
+
+//Toggle between the PC-Building
+
 (function getItems() {
   services.forEach((items) => {
     items.addEventListener("click", (e) => {
@@ -43,50 +55,103 @@ function gtItems(items) {
     }
   });
 }
-const form = document.getElementById("forms");
-function sendEmail() {
+
+//Emaill Integration
+
+function sendEmail(category) {
   let params = {
+    subject: "Get a quote",
     from: "mkvcinemas945@gmail.com",
     email:
       document.getElementById("emailfirst").value ||
       document.getElementById("emailsecond").value ||
       document.getElementById("emailthird").value,
-    subject: "Get a quote",
-    categoryforpcbuilding: document.getElementById("categoryforpcbuilding")
-      .value
-      ? "PC-Building"
-      : "--",
+    categoryforpcbuilding: category === "PC-Building" ? "PC-Building" : "--",
 
-    categoryforrepairs: document.getElementById("messageforpcbuilding").value
-      ? "Small-Repairs"
+    messageforpcbuilding:
+      category === "PC-Building"
+        ? document.getElementById("messageforpcbuilding").value
+        : "--",
+    options:
+      category === "PC-Building"
+        ? document.getElementById("pricecategoryforpcbuilding").value
+        : "--",
+    custom:
+      category === "PC-Building"
+        ? document.getElementById("custom").value
+        : "--",
+    exp:
+      category === "PC-Building"
+        ? document.getElementById("exp").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    categoryforupgrades: category === "Upgrades" ? "Upgrades" : "--",
+    cpu:
+      category === "Upgrades"
+        ? document.getElementById("cpu").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    gpu:
+      category === "Upgrades"
+        ? document.getElementById("gpu").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    cooler:
+      category === "Upgrades"
+        ? document.getElementById("cooler").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    ram:
+      category === "Upgrades"
+        ? document.getElementById("ram").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    casing:
+      category === "Upgrades"
+        ? document.getElementById("casing").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    motherboard:
+      category === "Upgrades"
+        ? document.getElementById("motherboard").checked
+          ? "Yes"
+          : "No"
+        : "--",
+    budget:
+      category === "Upgrades"
+        ? document.getElementById("budget").value
+          ? document.getElementById("budget").value + "$"
+          : "--"
+        : "--",
+    categoryforrepaires: category === "Small-Repairs" ? "Small-Repairs" : "--",
+
+    categoryofdevice:
+      category === "Small-Repairs"
+        ? document.getElementById("categoryofdevice").value
+        : "--",
+    issue: document.getElementById("issue").value
+      ? document.getElementById("issue").value
       : "--",
-    messageforpcbuilding: document.getElementById("messageforpcbuilding").value
-      ? ""
-      : "--",
-    options: document.getElementById("pricecategoryforpcbuilding").value,
-    custom: document.getElementById("custom").value
-      ? document.getElementById("custom").value
-      : "--",
-    exp: document.getElementById("exp").checked ? "Yes" : "No",
-    categoryforupgrades: document.getElementById("categoryforupgrades").value
-      ? "Upgrades"
-      : "--",
-    cpu: document.getElementById("cpu").checked ? "Yes" : "No",
-    gpu: document.getElementById("gpu").checked ? "Yes" : "No",
-    cooler: document.getElementById("cooler").checked ? "Yes" : "No",
-    ram: document.getElementById("ram").checked ? "Yes" : "No",
-    casing: document.getElementById("casing").checked ? "Yes" : "No",
-    motherboard: document.getElementById("motherboard").checked ? "Yes" : "No",
-    budget: document.getElementById("budget").value
-      ? document.getElementById("budget").value + "$"
-      : "No",
   };
   emailjs
     .send("service_tlxv4wh", "template_i41x9qp", params)
-    .then((message) => alert("mail sent successfully"));
+    .then((message) => alert("Email Sent"));
 }
-form.addEventListener("submit", (e) => {
+buildingForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  sendEmail();
+  sendEmail("PC-Building");
 });
-console.log(document.getElementById("options").value);
+upgradesForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail("Upgrades");
+});
+repairsForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail("Small-Repairs");
+});
