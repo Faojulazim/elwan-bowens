13dddf0b437a564a84e8c082d50ecabed18f5b2a
Faojul <faojulazim13@gmail.com>
2025-02-27 14:06:48 +0600

    new

--- script.js
+++ script.js
@@ -17,17 +17,6 @@ closeIcon.addEventListener("click", (e) => {
   closeIcon.classList.add("rotate-[90deg]");
   left.classList.add("translate-x-[-150%]");
 });
-// let i = 0;
-// const text = ["Build", "Create", "Make"];
-// setInterval(() => {
-//   if (i == text.length) {
-//     i = 0;
-//   } else {
-//     interval.innerText = text[i];
-//     i++;
-//   }
-// }, 2000);
-
 (function getItems() {
   services.forEach((items) => {
     items.addEventListener("click", (e) => {
@@ -54,3 +43,50 @@ function gtItems(items) {
     }
   });
 }
+const form = document.getElementById("forms");
+function sendEmail() {
+  let params = {
+    from: "mkvcinemas945@gmail.com",
+    email:
+      document.getElementById("emailfirst").value ||
+      document.getElementById("emailsecond").value ||
+      document.getElementById("emailthird").value,
+    subject: "Get a quote",
+    categoryforpcbuilding: document.getElementById("categoryforpcbuilding")
+      .value
+      ? "PC-Building"
+      : "--",
+
+    categoryforrepairs: document.getElementById("messageforpcbuilding").value
+      ? "Small-Repairs"
+      : "--",
+    messageforpcbuilding: document.getElementById("messageforpcbuilding").value
+      ? ""
+      : "--",
+    options: document.getElementById("pricecategoryforpcbuilding").value,
+    custom: document.getElementById("custom").value
+      ? document.getElementById("custom").value
+      : "--",
+    exp: document.getElementById("exp").checked ? "Yes" : "No",
+    categoryforupgrades: document.getElementById("categoryforupgrades").value
+      ? "Upgrades"
+      : "--",
+    cpu: document.getElementById("cpu").checked ? "Yes" : "No",
+    gpu: document.getElementById("gpu").checked ? "Yes" : "No",
+    cooler: document.getElementById("cooler").checked ? "Yes" : "No",
+    ram: document.getElementById("ram").checked ? "Yes" : "No",
+    casing: document.getElementById("casing").checked ? "Yes" : "No",
+    motherboard: document.getElementById("motherboard").checked ? "Yes" : "No",
+    budget: document.getElementById("budget").value
+      ? document.getElementById("budget").value + "$"
+      : "No",
+  };
+  emailjs
+    .send("service_tlxv4wh", "template_i41x9qp", params)
+    .then((message) => alert("mail sent successfully"));
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+});
+console.log(document.getElementById("options").value);
